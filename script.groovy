4763693fc171b6a77ba5e5967ed47ce1a352b359
vidhi-saxena02 <79373121+vidhi-saxena02@users.noreply.github.com>
2022-10-31 00:08:21 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
+def buildJar() {
     echo 'building the application'
+    sh 'mvn package'
 }  
 
-def testApp(){
+def buildImage(){
     echo 'testing the application'
+    withCredentials([usernamePassword(credentialsId: 'dockercred',passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        sh 'docker build -t vidhi2002/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push vidhi2002/demo-app:jma-2.0'
+    }    
 }
 
 def deployApp(){
     echo 'deploying the application' 
-    echo "deploying version ${params.VERSION}"
 }  
 
 return this
